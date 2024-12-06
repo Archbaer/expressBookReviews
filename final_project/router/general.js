d1b6bad9b9cfba0c0f5af6ce4280ad90afaf6c89
archbaer <pereirakebaili@gmail.com>
2024-12-06 19:17:08 +0000

    async await promises

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,34 +25,71 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/', async function (req, res) {
 
-  res.status(200).send(JSON.stringify(books));
+  try {
+    const booksList = await new Promise((resolve, reject) => {
+      resolve(books);
+    });
+
+    res.status(200).send(JSON.stringify(booksList));
+  } catch (error) {
+    res.status(500).send("Error fetching Books list.")
+  }
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', async function (req, res) {
   const isbn = req.params.isbn;
 
-  res.send(books[isbn]);
+  try {
+    const bookDetails = await new Promise((resolve, reject) => {
+      resolve(books[isbn]);
+    });
+
+    if(bookDetails) {
+      res.status(200).send(bookDetails);
+    } else {
+      res.status(404).send("Book not found");
+    }
+
+  } catch (error) {
+    res.status(500).send("Error fetching book details")
+  }
 });
 
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
   const author = req.params.author.trim(); // Trim whitespace
-  const autorEsc = booksArray.filter((book) =>
-    book.author.toLowerCase() === author.toLowerCase());
+  
+  try {
+    const authorBooks = await new Promise((resolve, reject) => {
+      const filteredBooks = booksArray.filter((book) =>
+        book.author.toLowerCase() === author.toLowerCase());
+      resolve(filteredBooks);
+    });
 
-  res.status(200).send(JSON.stringify(autorEsc));
+    res.status(200).send(JSON.stringify(authorBooks));
+  } catch (error) {
+    res.status(500).send("Error fetching books by author.");
+  }
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', async function (req, res) {
   const title = req.params.title.trim();
-  const titleEsc = booksArray.filter((book) => 
-    book.title.toLowerCase() === title.toLowerCase());
+  
+  try {
+    const titleBooks = await new Promise((resolve, reject) => {
+      const filteredBooks = booksArray.filter((book) => 
+        book.title.toLowerCase() === title.toLowerCase());
+      resolve(filteredBooks);
+    });
 
-  res.status(200).send(JSON.stringify(titleEsc));
+    res.status(200).send(JSON.stringify(titleBooks));
+  } catch (error) {
+    res.status(500).send("Error fetching books by title.");
+  }
 });
 
 //  Get book review
